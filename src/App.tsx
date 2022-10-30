7135592152dc809ee79599c1f15b359e9cb37092
JeweIxx <vhiunese@gmail.com>
2022-10-30 11:15:40 +0800

    added all the countries and a wip search system

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,70 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import React from 'react'
+import { nanoid } from "nanoid"
+import { countryDataInterface } from './interfaces/countryDataInterface'
+
 import Navbar from './components/Navbar'
+import SearchIcon from '@mui/icons-material/Search';
+import Country from './components/Country'
+
+
 function App() {
-  
+ 
+
+  const [countryData, setCountryData] = useState([] as countryDataInterface[])
+
+  useEffect(function() {
+    console.log(countryData);
+    
+    fetch("https://restcountries.com/v3.1/all")
+        .then(res => res.json())
+        .then(data => setCountryData(data))
+        
+  }, [])
+      
+  //this sorted everything but idk why really i thought i needed to setcountrydata to it 
+  countryData.sort(function(a: any,b : any){
+    if (a.name.common < b.name.common) {
+      return -1;
+    }
+    if (a.name.common > b.name.common) {
+      return 1;
+    }
+    return 0;  
+     
+  });
+
+
+const countrysList = countryData.map((country : any, index) => {
+  return (
+    <Country 
+      countryName={country.name.common}
+      countryImg={country.flags.png} 
+      countryRegion={country.region}
+      countryCapital={country.capital}
+      countryPopulation={country.population}
+      key={nanoid ()}
+    /> 
+)
+ 
+}) 
 
   return (
     <div className="App">
       <Navbar />
+      <div className="app__body">
+        <form className="app__form">
+          <div className="app__form-input-container">
+            <span ><SearchIcon className="app__form-input-search"/></span><input type="text" className="app__form-input"placeholder="Search for a country..."/>
+            <button type="submit"></button>
+          </div>
+
+        </form>
+        <div className="app__countrysList">
+
+          {countrysList}
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/components/Country.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./css-files/Country.css"
+
+function Country(props: { countryImg: string | undefined; countryName: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; countryPopulation: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; countryCapital: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; countryRegion: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined }) {
+  return (
+    <div className="country-card">
+     <img src={props.countryImg}/>
+      <div className='country-card__stats'>
+        <h2>{props.countryName}</h2>
+        <p><span style={{fontWeight: 600}}>Population: </span>{props.countryPopulation}</p>
+        <p><span style={{fontWeight: 600}}>Capital: </span> {props.countryCapital}</p>
+        <p><span style={{fontWeight: 600}}>Region: </span> {props.countryRegion}</p>
+
+      </div>
+    </div>
+  )
+}
+
+export default Country
