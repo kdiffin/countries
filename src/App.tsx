fd2d2894c856b56f3c6f15cc4ac2aae144ddfa14
JeweIxx <vhiunese@gmail.com>
2022-11-01 00:22:28 +0400

    added the countryinfo without responsive styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,159 +1,178 @@
-import { useState, useEffect } from 'react'
-import React from 'react'
-import { nanoid } from "nanoid"
-import { countryDataInterface } from './interfaces/countryDataInterface'
+import { useState, useEffect } from "react";
+import React from "react";
+import { nanoid } from "nanoid";
+import { countryDataInterface } from "./interfaces/countryDataInterface";
 
-import Navbar from './components/Navbar'
-import SearchIcon from '@mui/icons-material/Search';
-import Country from './components/Country'
-import CountryInfo from './components/CountryInfo'
+import Navbar from "./components/Navbar";
+import SearchIcon from "@mui/icons-material/Search";
+import Country from "./components/Country";
+import CountryInfo from "./components/CountryInfo";
 
-
-function App() { 
-  
-  
-    const [countryData, setCountryData] = useState<any[]>([])
-  const [countryDataMutable, setCountryDataMutable] = useState<any[]>([])
-  const [toggleDropdown, setToggleDropdown ] = useState<boolean>(false)
-  const [toggleCountryInfo, setToggleCountryInfo ] = useState<boolean>(false)
+function App() {
+  const [countryData, setCountryData] = useState<any[]>([]);
+  const [countryDataMutable, setCountryDataMutable] = useState<any[]>([]);
+  const [toggleDropdown, setToggleDropdown] = useState<boolean>(false);
+  const [toggleCountryInfo, setToggleCountryInfo] = useState<boolean>(false);
   const [countryId, setCountryId] = useState<number>(0);
-  const [regionSelected, setRegionSelected] = useState<boolean>(false)
-   
-  
-  
-  useEffect(function() {
+  const [regionSelected, setRegionSelected] = useState<boolean>(false);
+
+  useEffect(function () {
     console.log(countryData);
-    
+
     fetch("https://restcountries.com/v3.1/all")
-    .then(res => res.json())
-    .then(data => setCountryData(data))
-    
-     
-  }, [])
-  
-  
-  countryData.sort(function(a,b ){
+      .then((res) => res.json())
+      .then((data) => setCountryData(data));
+  }, []);
+
+  countryData.sort(function (a, b) {
     if (a.name.common < b.name.common) {
       return -1;
     }
-    if (a.name.common > b.name.common) { 
+    if (a.name.common > b.name.common) {
       return 1;
     }
-    return 0;  
-    
+    return 0;
   });
-  
-  
+
   const countrysList = countryData.map((country) => {
     return (
-      <Country 
-      countryName={country.name.common}
-      countryImg={country.flags.png} 
-      countryRegion={country.region}
-      countryCapital={country.capital}
-      countryPopulation={country.population}
-      key={nanoid()}
-      showCountryInfo={showCountryInfo}
-      name={country.name.common}
-      
-      /> 
-      )
-    })
-    
-    const countryListFiltered = countryDataMutable.map((country) => {
-      return (
-        <Country 
+      <Country
         countryName={country.name.common}
-        countryImg={country.flags.png} 
+        countryImg={country.flags.png}
         countryRegion={country.region}
         countryCapital={country.capital}
         countryPopulation={country.population}
         key={nanoid()}
         showCountryInfo={showCountryInfo}
         name={country.name.common}
-        />
-        )
-      })
-      
-    
-    function selectRegion(countryRegion : string){
-      setCountryDataMutable(countryData.filter((country) => country.region == countryRegion))
-      setRegionSelected(true) 
-    }
-    
-    
-    
-    
-    function showCountryInfo(event: any, countryName: any) {
-      
-      setToggleCountryInfo(true)
-      const boolList = countryData.map((country ) => country.name.common === countryName);
-      const index: number = boolList.indexOf(true);
-      setCountryId(index);
-      
-       
-    }
-    
-    function hideCountryInfo() {
-      setToggleCountryInfo(false)
-    }
-    
+      />
+    );
+  });
 
-
-    
-
-    
-    
-      
-    
+  const countryListFiltered = countryDataMutable.map((country) => {
     return (
-      <div className="App">
+      <Country
+        countryName={country.name.common}
+        countryImg={country.flags.png}
+        countryRegion={country.region}
+        countryCapital={country.capital}
+        countryPopulation={country.population}
+        key={nanoid()}
+        showCountryInfo={showCountryInfo}
+        name={country.name.common}
+      />
+    );
+  });
+
+  function selectRegion(countryRegion: string) {
+    setCountryDataMutable(
+      countryData.filter((country) => country.region == countryRegion)
+    );
+    setRegionSelected(true);
+  }
+
+  function showCountryInfo(event: any, countryName: any) {
+    setToggleCountryInfo(true);
+    const boolList = countryData.map(
+      (country) => country.name.common === countryName
+    );
+    const index: number = boolList.indexOf(true);
+    setCountryId(index);
+  }
+
+  function hideCountryInfo() {
+    setToggleCountryInfo(false);
+  }
+
+  return (
+    <div className="App">
       <Navbar />
-      {!toggleCountryInfo ? <div className="app__body">
+      {!toggleCountryInfo ? (
+        <div className="app__body">
+          <div className="app__form">
+            <div className="app__form-input-container">
+              <span>
+                <SearchIcon className="app__form-input-search" />
+              </span>
+              <input
+                type="text"
+                className="app__form-input"
+                placeholder="Search for a country..."
+              />
+              <button type="submit"></button>
+            </div>
 
-        <div className="app__form">
-          <div className="app__form-input-container">
-            <span ><SearchIcon className="app__form-input-search"/></span><input type="text" className="app__form-input"placeholder="Search for a country..."/>
-            <button type="submit"></button>
+            <div className="app__form-select">
+              <h4
+                style={{ fontWeight: 400 }}
+                onClick={() => setToggleDropdown(!toggleDropdown)}
+              >
+                Select by region:
+              </h4>
+              {toggleDropdown && (
+                <ul>
+                  <li onClick={() => setRegionSelected(false)}>Any region</li>
+                  <li onClick={() => selectRegion("Asia")}>Asia</li>
+                  <li onClick={() => selectRegion("Europe")}>Europe</li>
+                  <li onClick={() => selectRegion("Africa")}>Africa</li>
+                  <li onClick={() => selectRegion("Oceania")}>Oceania</li>
+                  <li onClick={() => selectRegion("Americas")}>Americas</li>
+                  <li onClick={() => selectRegion("Antarctic")}>Antarctic</li>
+                </ul>
+              )}
+            </div>
           </div>
-
-          <div className="app__form-select">
-            <h4  style={{fontWeight: 400}}onClick={() => setToggleDropdown(!toggleDropdown)}>Select by region:</h4>
-            {toggleDropdown && <ul>
-              <li onClick={() => setRegionSelected(false)} >Any region</li>
-              <li onClick={() => selectRegion("Asia")} >Asia</li>
-              <li onClick={() => selectRegion("Europe")}>Europe</li>
-              <li onClick={() =>selectRegion("Africa")}>Africa</li>
-              <li onClick={() =>selectRegion("Oceania")}>Oceania</li>
-              <li onClick={() =>selectRegion("Americas")}>Americas</li>
-              <li onClick={() =>selectRegion("Antarctic")}>Antarctic</li>
-            </ul>}
+          <div className="app__countrysList">
+            {!regionSelected ? countrysList : countryListFiltered}
           </div>
-
         </div>
-        <div className="app__countrysList">
-           {!regionSelected ? countrysList : countryListFiltered}
-        </div>  
-      </div> : 
-      <CountryInfo   
-        countryImg={countryData[countryId].flags.png} 
-        countryIndependant={countryData[countryId].independent}
-        hideCountryInfo={hideCountryInfo}
-        countryName={countryData[countryId].name.common}
-        countryOfficialName={countryData[countryId].name.official}
-        countryNativeName={countryData[countryId].name.nativeName === undefined ? "No native name..." : countryData[countryId].name.nativeName[Object.keys(countryData[countryId].name.nativeName)[0]].common}
-        countryPopulation={countryData[countryId].population}
-        countryRegion={countryData[countryId].region}
-        countrySubRegion={countryData[countryId].subregion}
-        countryCapital={countryData[countryId].capital === undefined ? "No capital" : countryData[countryId].capital[0]}
-        countryDomain={countryData[countryId].tld} 
-        countryCurrency={countryData[countryId].currencies === undefined ? "No currency..." : countryData[countryId].currencies[Object.keys(countryData[countryId].currencies)[0]][Object.keys(countryData[countryId].currencies[Object.keys(countryData[countryId].currencies)[0]])[0]]}
-          
-        countryLanguages={countryData[countryId].languages === undefined ? "No language..." : countryData[countryId].languages[Object.keys(countryData[countryId].languages)[0]]}
-    
-      />} 
-    </div> 
-  )
+      ) : (
+        <CountryInfo
+          countryImg={countryData[countryId].flags.png}
+          countryIndependant={countryData[countryId].independent}
+          hideCountryInfo={hideCountryInfo}
+          countryName={countryData[countryId].name.common}
+          countryOfficialName={countryData[countryId].name.official}
+          countryNativeName={
+            countryData[countryId].name.nativeName === undefined
+              ? "No native name..."
+              : countryData[countryId].name.nativeName[
+                  Object.keys(countryData[countryId].name.nativeName)[0]
+                ].common
+          }
+          countryPopulation={countryData[countryId].population}
+          countryRegion={countryData[countryId].region}
+          countrySubRegion={countryData[countryId].subregion}
+          countryCapital={
+            countryData[countryId].capital === undefined
+              ? "No capital"
+              : countryData[countryId].capital[0]
+          }
+          countryDomain={countryData[countryId].tld}
+          countryCurrency={
+            countryData[countryId].currencies === undefined
+              ? "No currency..."
+              : countryData[countryId].currencies[
+                  Object.keys(countryData[countryId].currencies)[0]
+                ][
+                  Object.keys(
+                    countryData[countryId].currencies[
+                      Object.keys(countryData[countryId].currencies)[0]
+                    ]
+                  )[0]
+                ]
+          }
+          countryLanguages={
+            countryData[countryId].languages === undefined
+              ? "No language..."
+              : countryData[countryId].languages[
+                  Object.keys(countryData[countryId].languages)[0]
+                ]
+          }
+        />
+      )}
+    </div>
+  );
 }
- 
-export default App
+
+export default App;
--- src/components/CountryInfo.tsx
+++ src/components/CountryInfo.tsx
@@ -1,31 +1,161 @@
-import React from 'react'
-import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
+import React from "react";
+import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
+import "./css-files/CountryInfo.css";
 
-function CountryInfo(props: { hideCountryInfo: React.MouseEventHandler<HTMLDivElement> | undefined; countryImg: string | undefined; countryName: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; countryOfficialName: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; countryNativeName: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; countryIndependant: any; countryPopulation: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; countryRegion: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; countrySubRegion: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | null | undefined; countryCapital: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; countryDomain: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; countryCurrency: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; countryLanguages: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; }) {
+function CountryInfo(props: {
+  hideCountryInfo: React.MouseEventHandler<HTMLDivElement> | undefined;
+  countryImg: string | undefined;
+  countryName:
+    | string
+    | number
+    | boolean
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+    | React.ReactFragment
+    | React.ReactPortal
+    | null
+    | undefined;
+  countryOfficialName:
+    | string
+    | number
+    | boolean
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+    | React.ReactFragment
+    | React.ReactPortal
+    | null
+    | undefined;
+  countryNativeName:
+    | string
+    | number
+    | boolean
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+    | React.ReactFragment
+    | React.ReactPortal
+    | null
+    | undefined;
+  countryIndependant: any;
+  countryPopulation:
+    | string
+    | number
+    | boolean
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+    | React.ReactFragment
+    | React.ReactPortal
+    | null
+    | undefined;
+  countryRegion:
+    | string
+    | number
+    | boolean
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+    | React.ReactFragment
+    | React.ReactPortal
+    | null
+    | undefined;
+  countrySubRegion:
+    | string
+    | number
+    | boolean
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+    | React.ReactFragment
+    | null
+    | undefined;
+  countryCapital:
+    | string
+    | number
+    | boolean
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+    | React.ReactFragment
+    | React.ReactPortal
+    | null
+    | undefined;
+  countryDomain:
+    | string
+    | number
+    | boolean
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+    | React.ReactFragment
+    | React.ReactPortal
+    | null
+    | undefined;
+  countryCurrency:
+    | string
+    | number
+    | boolean
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+    | React.ReactFragment
+    | React.ReactPortal
+    | null
+    | undefined;
+  countryLanguages:
+    | string
+    | number
+    | boolean
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+    | React.ReactFragment
+    | React.ReactPortal
+    | null
+    | undefined;
+}) {
   return (
     <div className="country-info">
-        <div onClick={props.hideCountryInfo} className="country-info__back">
-            <KeyboardBackspaceIcon /> back
-        </div>
-        <div className='country-info__body'>
-          <img src={props.countryImg}/>
-          <h2>{props.countryName}</h2>
-          <div className="country-info__stats">
-            <p><span>Official Name: </span>{props.countryOfficialName}</p>
-            <p><span>Native Name: </span>{ props.countryNativeName}</p>
-            <p><span>Independant: </span>{props.countryIndependant ? "true" : "false"}</p>
+      <div onClick={props.hideCountryInfo} className="country-info__back">
+        <KeyboardBackspaceIcon /> <span>Back</span>
+      </div>
+      <div className="country-info__body">
+        <img src={props.countryImg} />
+        <div className="country-info__stats">
+          <div className="country-info__stats-first">
+            <h1>{props.countryName}</h1>
+            <p>
+              <span>Official Name: </span>
+              {props.countryOfficialName}
+            </p>
+            <p>
+              <span>Native Name: </span>
+              {props.countryNativeName}
+            </p>
+            <p>
+              <span>Population: </span>
+              {props.countryPopulation}
+            </p>
+            <p>
+              <span>Region: </span>
+              {props.countryRegion}
+            </p>
+            <p>
+              <span>Sub Region: </span>
+              {props.countrySubRegion === undefined
+                ? "No sub region..."
+                : props.countrySubRegion}
+            </p>
+            <p>
+              <span>Capital: </span>
+              {props.countryCapital}
+            </p>
+          </div>
 
-            <p><span>Population: </span>{props.countryPopulation}</p>
-            <p><span>Region: </span>{props.countryRegion}</p>
-            <p><span>Sub Region: </span>{props.countrySubRegion === undefined ? "No sub region..." : props.countrySubRegion}</p>
-            <p><span>Capital: </span>{props.countryCapital}</p>
-            <p><span>Domain name: </span>{props.countryDomain}</p>
-            <p><span>Currencies: </span>{props.countryCurrency}</p>
-            <p><span>Main Language: </span>{props.countryLanguages }</p>
+          <div className="country-info__stats-second">
+            <p>
+              <span>Domain name: </span>
+              {props.countryDomain}
+            </p>
+            <p>
+              <span>Currencies: </span>
+              {props.countryCurrency}
+            </p>
+            <p>
+              <span>Independant: </span>
+              {props.countryIndependant ? "yeah" : "nah"}
+            </p>
+            <p>
+              <span>Main Language: </span>
+              {props.countryLanguages}
+            </p>
           </div>
         </div>
-    </div>  
-  )
+      </div>
+    </div>
+  );
 }
 
-export default CountryInfo
+export default CountryInfo;
