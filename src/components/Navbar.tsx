101ac896bb302249c49f4873de3d650078f17657
JeweIxx <vhiunese@gmail.com>
2022-11-10 00:45:46 +0400

    added ternary to light/dark mode toggle

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./css-files/Navbar.css";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
+import LightModeIcon from "@mui/icons-material/LightMode";
 function Navbar(props: {
   toggleDarkMode: any;
   switchDarkMode: React.MouseEventHandler<HTMLDivElement> | undefined;
@@ -10,7 +11,17 @@ function Navbar(props: {
       <h1>Where in the world?</h1>
 
       <div className="navbar__mode-button" onClick={props.switchDarkMode}>
-        <DarkModeIcon /> <h4>Dark Mode</h4>
+        {props.toggleDarkMode ? (
+          <>
+            <LightModeIcon />
+            <h4>Light Mode</h4>
+          </>
+        ) : (
+          <>
+            <DarkModeIcon />
+            <h4>Dark Mode</h4>
+          </>
+        )}
       </div>
     </nav>
   );
